ca1f533aa1d6f0fc6c55ea6d33fe9b9ad0fd4dd9
dingzhaoliang <dingzhaoliang@naboo-game.com>
2022-07-22 18:27:28 +0800

    create unity project

---
+++ RpgDemoProject/Assets/Scripts/Battle/BattleGame.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleGame : MonoBehaviour
+{
+    private bool _pause = false;
+	private void Awake()
+	{
+		
+	}
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_pause)
+            return;
+
+        UpdateGameInput();
+    }
+
+    void UpdateGameInput()
+	{
+        var dir = KeyBoardUtils.GetKeyBoardMoveDirection();
+	}
+}
---
+++ RpgDemoProject/Assets/Scripts/Utils/KeyBoardUtils.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyBoardUtils
+{
+	public static Vector3 GetKeyBoardMoveDirection()
+	{
+		bool keyUp = false;
+		bool keyDown = false;
+		bool keyLeft = false;
+		bool keyRight = false;
+		Vector3 getDirection = Vector3.zero;
+
+		if (Input.GetKey(KeyCode.W))
+		{
+			keyUp = true;
+		}
+		if (Input.GetKeyUp(KeyCode.W))
+		{
+			keyUp = false;
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			keyDown = true;
+		}
+		if (Input.GetKeyUp(KeyCode.S))
+		{
+			keyDown = false;
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			keyLeft = true;
+		}
+		if (Input.GetKeyUp(KeyCode.A))
+		{
+			keyLeft = false;
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			keyRight = true;
+		}
+		if (Input.GetKeyUp(KeyCode.D))
+		{
+			keyRight = false;
+		}
+
+		if (keyUp)
+		{
+			if (keyLeft)
+			{
+				getDirection = GetDirection(135);
+			}
+			else if (keyRight)
+			{
+				getDirection = GetDirection(45);
+			}
+			else
+			{
+				getDirection = GetDirection(90);
+			}
+		}
+		else if (keyDown)
+		{
+			if (keyLeft)
+			{
+				getDirection = GetDirection(225);
+			}
+			else if (keyRight)
+			{
+				getDirection = GetDirection(315);
+			}
+			else
+			{
+				getDirection = GetDirection(270);
+			}
+		}
+		else if (keyLeft)
+		{
+			getDirection = GetDirection(180);
+		}
+		else if (keyRight)
+		{
+			getDirection = GetDirection(360);
+		}
+
+		return getDirection;
+	}
+
+	public static Vector3 GetDirection(float angle)
+	{
+		Vector3 getDirection = Vector3.zero;
+		getDirection.x = (float)Math.Cos(angle * Math.PI / 180);
+		getDirection.z = (float)Math.Sin(angle * Math.PI / 180);
+		return getDirection;
+	}
+}
