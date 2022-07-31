79b6508a6e1c24d0c363f326055fad22a8fd7376
dingzhaoliang <dzl7918209@163.com>
2022-08-01 00:28:27 +0800

    技能编辑器

---
+++ RpgDemoProject/Assets/SkillEditor/Scripts/SkillEvents/PlaySound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using Sirenix.OdinInspector;
+
+namespace RpgDemo
+{
+	public class PlaySound : SkillEventBase
+	{
+		[SkillCustomParams]
+		[FoldoutGroup("CounstomParams")]
+		public string animation;
+	}
+}
+
---
+++ RpgDemoProject/Assets/SkillEditor/Scripts/SkillEvents/SkillEventBase.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEditor;
+using UnityEngine.Timeline;
+using System;
+using Sirenix.OdinInspector;
+
+namespace RpgDemo
+{
+    [HideInMenu]
+    public class SkillEventBase : PlayableAsset
+    {
+        [SkillBaseParams]
+        [FoldoutGroup("BaseParams")]
+        [InlineButton("New")]
+        [InlineButton("Copy")]
+        public long eventID;
+
+        [SkillBaseParams]
+        [FoldoutGroup("BaseParams")]
+        [InlineButton("Reset")]
+        [InlineButton("Paste")]
+        [DisplayAsString]
+        public long preID;
+
+        [SkillBaseParams]
+        [FoldoutGroup("BaseParams")]
+        public int loopCount;
+
+        [SkillBaseParams]
+        [FoldoutGroup("BaseParams")]
+        public float loopInterval;
+
+        [SkillBaseParams]
+        [Range(0, 1)]
+
+        [FoldoutGroup("BaseParams")]
+        public float prob = 1f;
+
+        public SkillEventBase()
+        {
+            NewEventID();
+        }
+
+        public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+        {
+            return Playable.Null;
+        }
+
+        public void NewEventID()
+        {
+            eventID = Convert.ToInt64((DateTime.UtcNow - new DateTime(2022, 1, 1, 0, 0, 0, 0)).TotalMilliseconds);
+        }
+
+        private void New()
+        {
+            NewEventID();
+        }
+
+        private void Copy()
+        {
+            GUIUtility.systemCopyBuffer = eventID.ToString();
+        }
+
+        private void Paste()
+        {
+            long id = 0;
+            if (long.TryParse(GUIUtility.systemCopyBuffer, out id))
+            {
+                preID = id;
+            }
+        }
+
+        private void Reset()
+        {
+            preID = 0;
+        }
+    }
+}
